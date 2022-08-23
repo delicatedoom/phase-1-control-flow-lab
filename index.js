2449eff9e6177381265ed5e5465e6eaf203b5e1c
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-08-23 10:38:34 -0600

    update

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  let results
+  if(ride <= 400) {
+    results = 'This one is on me!'
+  }
+  
+  else if (ride >400 && ride < 2000) {
+    results = 'That will be twenty bucks.'
+  }
+
+  else if (ride > 2000 && ride < 2500) {
+    results = "I will gladly take your thirty bucks."
+  }
+
+  else if(ride > 2500) {
+    results = "No can do."
+  };
+
+  return results
+};
+
+function ternaryCheckCity(city) {
+  let answer;
+
+  city === "NYC" ? answer = "Ok, sounds good." : answer = "No go."
+
+  return answer
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let answer
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === "generous"){
+    answer = "Thank you so much."
+  }
+
+  else if (tip === "not as generous"){
+    answer = "Thank you."
+  }
+
+  else {
+    answer = "Bye."
+  }
+
+  return answer
 }
